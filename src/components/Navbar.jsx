9fb544c142ad10bd6a20c8f37f3e94fddc818588
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-05-23 17:21:10 +0600

    2nd Coments

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1 className="text-green-500">ACME Navbar</h1>
+            
+        </div>
+    );
+};
+
+export default Navbar;
