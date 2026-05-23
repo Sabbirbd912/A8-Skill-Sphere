ac48502406de6fc02c87ec8fb3631142295c88f6
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-05-24 00:01:58 +0600

    json data added

---
+++ src/components/Banner.jsx
@@ -0,0 +1,27 @@
+const Banner = () => {
+  return (
+    <div
+      className="relative bg-cover bg-center h-[450px]"
+      style={{
+        backgroundImage: "url('/banner-photo.jpg')",
+      }}
+    >
+
+      <div className="absolute inset-0 bg-black/60"></div>
+
+      <div className="relative z-10 flex flex-col justify-center items-center h-full text-white text-center px-4">
+        
+        <h1 className="text-5xl md:text-6xl font-bold mb-6">
+          Upgrade Your Skills Today 🚀
+        </h1>
+
+        <p className="text-lg md:text-2xl max-w-2xl">
+          Learn from industry experts and build your future with SkillSphere
+        </p>
+
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,44 @@
-
+"use client";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h1 className="text-green-500">ACME Navbar</h1>
-            
+  return (
+    <div className="border-b px-2">
+      <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
+        <div className="flex gap-2 items-center">
+            <Image src={"/logo.png"} alt="logo" loading="eager" width={30} height={30} className="object-cover h-auto w-auto" />
+          <h3 className="font-black text-lg">SkillSphere</h3>
         </div>
-    );
+
+        <ul className="flex items-center gap-5 text-sm">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/all-photos"}>Courses</Link>
+          </li>
+          <li>
+            <Link href={"/pricing"}>My Profile</Link>
+          </li>
+          <li>
+            <Link href={"/profile"}>Profile</Link>
+          </li>
+        </ul>
+
+        <div className="flex gap-4">
+          <ul className="flex items-center  text-sm">
+            {/* <li>
+              <Link href={"/signup"}>SignUp</Link>
+            </li> */}
+            <li>
+              <Link href={"/signin"}>Sign In</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
