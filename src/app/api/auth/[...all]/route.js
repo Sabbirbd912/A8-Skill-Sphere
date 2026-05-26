8578f4c676789e26ce9e9b46493772f0b7d703e0
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-05-26 19:35:13 +0600

    tryint go solve problem

--- src/app/api/auth/[...all]/route.js
+++ src/app/api/auth/[...all]/route.js
@@ -1,9 +1,4 @@
-export const runtime = "nodejs";
-
-import { auth } from "@/lib/auth";
+import {auth} from "../../../../lib/auth"
 import { toNextJsHandler } from "better-auth/next-js";
 
-const handler = toNextJsHandler(auth);
-
-export const GET = handler.GET;
-export const POST = handler.POST;
+export const { POST, GET } = toNextJsHandler(auth);
