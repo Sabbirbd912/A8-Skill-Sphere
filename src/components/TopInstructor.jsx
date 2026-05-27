98ca8d207195577f68a2e045b45f4d6b304783d5
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-05-27 10:40:56 +0600

    all features checked done

--- src/components/TopInstructor.jsx
+++ src/components/TopInstructor.jsx
@@ -4,7 +4,7 @@ const TopInstructor = () => {
     const topInstructor = data.instructors?.slice(0, 3) || [];
     return (
         <div>
-            <h2 className="bg-linear-to-tr from-black via-blue-800 to-blue-500 bg-clip-text text-transparent text-3xl font-extrabold py-4 text-center">Our Popular Instructor</h2>
+            <h2 className="bg-linear-to-tr from-black via-blue-800 to-blue-500 bg-clip-text text-transparent text-3xl font-extrabold py-4 text-center">Popular Instructor</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-5 md:gap-6 mt-4 sm:mt-5 md:mt-6 justify-items-center sm:justify-items-stretch">
                 {topInstructor.map(instructor => (
                     <InstructorCard key={instructor.id} instructor={instructor} />
--- src/lib/auth.client.js
+++ src/lib/auth.client.js
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://a8-skill-sphere.vercel.app"
+    baseURL: process.env.NEXT_PUBLIC_SITE_URL || window.location.origin
 })
