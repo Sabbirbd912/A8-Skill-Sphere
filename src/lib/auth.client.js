7d202990d9d5f0142f44f850dc4fdab614a14fa2
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-05-27 13:10:12 +0600

    signIn issues solving

--- src/lib/auth.client.js
+++ src/lib/auth.client.js
@@ -1,5 +1,4 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-
-    baseURL: process.env.NEXT_PUBLIC_SITE_URL || (typeof window !== "undefined" ? window.location.origin : "")
+    /** The base URL of the server (optional if you're using the same domain) */
 })
