5a06b7a20ffc8375eb9a3dc6f852fc0e0ad2c8cb
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-05-27 12:30:41 +0600

    fix: auth trusted headers for vercel

--- src/lib/auth.client.js
+++ src/lib/auth.client.js
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.NEXT_PUBLIC_SITE_URL || window.location.origin
+
+    baseURL: process.env.NEXT_PUBLIC_SITE_URL || (typeof window !== "undefined" ? window.location.origin : "")
 })
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -15,5 +15,8 @@ export const auth = betterAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }
+  },
+  advanced: {
+    trustedHeaders: ["x-forwarded-host", "x-forwarded-proto", "origin"]
   }
 });
