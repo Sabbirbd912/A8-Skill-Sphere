3c1e9525cb8d96ebaabffba8ff3e7010d7ebaf1c
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-05-28 00:18:40 +0600

    MongoDB Account Changed

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,7 +3,8 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db("skillSphere");
+
+const db = client.db("a8-skill-spare");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
